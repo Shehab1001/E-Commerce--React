f64d83516040b38a74912eb0c20b387cf24dc514
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2023-12-22 00:28:11 +0200

    categories

---
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import { useQueries, useQuery } from 'react-query';
+
+export default function CategorySlider() {
+   
+   function getCategories()
+    let x = useQuery()
+   
+   return <>
+
+    
+    </>
+}
+
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import react from 'react';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
+import CategorySlider from '../CategorySlider/CategorySlider';
 
 export default function Home() {
     return <>
-
+        <CategorySlider/>        
         <FeaturedProducts />
     </>
 }
